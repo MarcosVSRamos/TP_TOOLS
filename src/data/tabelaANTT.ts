17bbddd10d1b6aed9cac5a1c213cd2705ba89686
MarcosDevRamos <santosramosmarcosvinicius@gmail.com>
2026-07-18 09:30:11 -0400

    atualização de coeficientes

--- src/data/tabelaANTT.ts
+++ src/data/tabelaANTT.ts
@@ -5,35 +5,35 @@ export const tabelaANTT: TabelaANTT[] = [
     composicao: 'LS 32t',
     eixos: 6,
     capacidade: 32,
-    ccd: 7.4124,
-    cc: 648.95
+    ccd: 7.3547,
+    cc: 671.93
   },
   {
     composicao: 'Bitrem 37t',
     eixos: 7,
     capacidade: 37,
-    ccd: 8.1252,
-    cc: 803.22
+    ccd: 8.0927,
+    cc: 831.66
   },
   {
     composicao: 'LS 4Eixo 40t',
     eixos: 7,
     capacidade: 40,
-    ccd: 8.1252,
-    cc: 803.22
+    ccd: 8.0927,
+    cc: 831.66
   },
   {
     composicao: 'RodoTrem 48t',
     eixos: 9,
     capacidade: 48,
-    ccd: 9.2466,
-    cc: 872.44
+    ccd: 9.2027,
+    cc: 903.32
   },
   {
     composicao: 'RodoCurto 50t',
     eixos: 9,
     capacidade: 50,
-    ccd: 9.2466,
-    cc: 872.44
+    ccd: 9.2027,
+    cc: 903.32
   }
 ]
